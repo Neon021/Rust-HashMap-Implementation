416be389803b489812e1ce3a2a67bab8fe98cbcd
root <root@Furkan>
2024-02-05 17:35:44 +0300

    IntoIterator implemented

--- examples/std1.rs
+++ examples/std1.rs
@@ -1,52 +1,39 @@
 extern crate hashmap;
 use hashmap::HashMap;
 
-// Type inference lets us omit an explicit type signature (which
-// would be `HashMap<String, String>` in this example).
-let mut book_reviews = HashMap::new();
+fn main() {
+    // type inference lets us omit an explicit type signature (which
+    // would be `HashMap<&str, &str>` in this example).
+    let mut book_reviews = HashMap::new();
 
-// Review some books.
-book_reviews.insert(
-    "Adventures of Huckleberry Finn".to_string(),
-    "Haven't read.".to_string(),
-);
-book_reviews.insert(
-    "Grimms' Fairy Tales".to_string(),
-    "Haven't read either :3.".to_string(),
-);
-book_reviews.insert(
-    "Pride and Prejudice".to_string(),
-    "Fucking victrian morale romance.".to_string(),
-);
-book_reviews.insert(
-    "The Adventures of Sherlock Holmes".to_string(),
-    "Haven't read :3:3.".to_string(),
-);
+    // review some books.
+    book_reviews.insert("Adventures of Huckleberry Finn", "My favorite book.");
+    book_reviews.insert("Grimms' Fairy Tales", "Masterpiece.");
+    book_reviews.insert("Pride and Prejudice", "Very enjoyable.");
+    book_reviews.insert("The Adventures of Sherlock Holmes", "Eye lyked it alot.");
 
-// Check for a specific one.
-// When collections store owned values (String), they can still be
-// queried using references (&str).
-if !book_reviews.contains_key("Les Misérables") {
-    println!("We've got {} reviews, but Les Misérables ain't one.",
-             book_reviews.len());
-}
-
-// oops, this review has a lot of spelling mistakes, let's delete it.
-book_reviews.remove("The Adventures of Sherlock Holmes");
-
-// Look up the values associated with some keys.
-let to_find = ["Pride and Prejudice", "Alice's Adventure in Wonderland"];
-for &book in &to_find {
-    match book_reviews.get(book) {
-        Some(review) => println!("{book}: {review}"),
-        None => println!("{book} is unreviewed.")
+    // check for a specific one.
+    if !book_reviews.contains_key("Les Misérables") {
+        println!(
+            "We've got {} reviews, but Les Misérables ain't one.",
+            book_reviews.len()
+        );
     }
-}
 
-// Look up the value for a key (will panic if the key is not found).
-println!("Review for Jane: {}", book_reviews["Pride and Prejudice"]);
+    // oops, this review has a lot of spelling mistakes, let's delete it.
+    book_reviews.remove("The Adventures of Sherlock Holmes");
 
-// Iterate over everything.
-for (book, review) in &book_reviews {
-    println!("{book}: \"{review}\"");
+    // look up the values associated with some keys.
+    let to_find = ["Pride and Prejudice", "Alice's Adventure in Wonderland"];
+    for book in &to_find {
+        match book_reviews.get(book) {
+            Some(review) => println!("{}: {}", book, review),
+            None => println!("{} is unreviewed.", book),
+        }
+    }
+
+    // iterate over everything.
+    for (book, review) in &book_reviews {
+        println!("{}: \"{}\"", book, review);
+    }
 }
